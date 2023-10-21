f8167cb9b581cb87d74318535332370cbc3b3699
Sarah Asad <sarah_asad@live.com>
2023-10-21 09:03:45 -0400

    YAY im not a server but im always up for connectio

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import RegisterOrg from './OrganizationsUI/RegisterOrg';
 import RegisterRes from './RestaurantUI/RegisterRes'; 
 import RestaurantMapView from './OrganizationsUI/RestaurantTableView';
 import SignIn from './SharedUI/SignIn';
+import AddMenu from './RestaurantUI/AddMenu'
 
 const Stack = createNativeStackNavigator(); 
 
@@ -20,6 +21,7 @@ export default function App() {
         <Stack.Screen name="RegisterOrg" component={RegisterOrg} options={{title: "Register"}}></Stack.Screen>
         <Stack.Screen name="MapScreen" component={RestaurantMapView} options={{title: "Map View"}}></Stack.Screen> 
         <Stack.Screen name="SignIn" component={SignIn} options={{title: "Sign In"}}></Stack.Screen>
+        <Stack.Screen name='AddMenu' component={AddMenu} options={{title: "Add Menu"}}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ RestaurantUI/AddMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const AddMenu = ({navigation}: {navigation: any}) =>{
+    return(
+        <View>
+            <Text>Add your menu for fast and easy food postings!</Text>
+        </View>
+
+    )
+}
+
+export default AddMenu; 
