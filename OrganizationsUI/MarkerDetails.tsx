8e49d321fb1a20612f5a3c86f4ad7235a387b50e
Sarah Asad <sarah_asad@live.com>
2023-10-22 02:49:51 -0400

    server changesssss

--- OrganizationsUI/MarkerDetails.tsx
+++ OrganizationsUI/MarkerDetails.tsx
@@ -1,12 +1,65 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const MarkerDetails = ({route, navigation}: {route: any, navigation:any}) =>{
-    const {restaurantData} = route.params;
-    return(
-        <Text>hello</Text>
-    )
-}
+// Import your getNutrients function or the module where it is defined
+import { getNutrients } from '../nutrition';
 
+const MarkerDetails = ({ route }) => {
+  const { restaurant, menu } = route.params;
+  const [nutritionalDetails, setNutritionalDetails] = useState(null);
 
-export default MarkerDetails; 
+  useEffect(() => {
+    // Assuming that menu.item contains the menu item name
+    if (menu.item) {
+      // Call the getNutrients function with the menu item name
+      getNutrients(menu.item)
+        .then(nutritionalData => {
+          setNutritionalDetails(nutritionalData);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [menu.item]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.restaurantName}>{restaurant.name}</Text>
+      <Text style={styles.menuTitle}>Menu:</Text>
+      <Text style={styles.menuItem}>{menu.item}</Text>
+      <Text style={styles.menuTitle}>Nutritional Details:</Text>
+      {nutritionalDetails && Object.keys(nutritionalDetails).map(nutrientName => (
+        <Text key={nutrientName} style={styles.nutrientItem}>
+          {nutrientName}: {nutritionalDetails[nutrientName]}
+        </Text>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  restaurantName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  menuTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 8,
+  },
+  menuItem: {
+    fontSize: 16,
+    marginTop: 4,
+  },
+  nutrientItem: {
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
+
+export default MarkerDetails;
